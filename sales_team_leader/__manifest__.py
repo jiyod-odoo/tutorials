f9fe7ce5b230b9a57fa42460b2dcf6c8f58dbade
JIYOD <jiyod@odoo.com>
2025-11-17 17:02:04 +0800

    case-study-security

---
+++ sales_team_leader/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'sales_team_leader',
+    'depends': ['sale_management', 'sales_team'],
+    'author': 'JIYOD',
+    'data': [
+        'security/sales_team_leader_security.xml',
+        'security/ir.model.access.csv',
+    ],
+    'license': 'AGPL-3',
+
+}
